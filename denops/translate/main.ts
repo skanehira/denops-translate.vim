f470eae84f5ac0e52cd30b1053b357758169c3b7
skanehira <sho19921005@gmail.com>
2021-09-28 21:30:18 +0900

    make init

--- denops/translate/main.ts
+++ denops/translate/main.ts
@@ -2,11 +2,11 @@ import { Denops, isString } from "./deps.ts";
 
 export async function main(denops: Denops): Promise<void> {
   await denops.cmd(
-    `command! -nargs=1 Hello call denops#notify("${denops.name}", "hello", [<f-args>])`,
+    `command! -nargs=1 Translate call denops#notify("${denops.name}", "translate", [<f-args>])`,
   );
 
   denops.dispatcher = {
-    async hello(arg: unknown): Promise<void> {
+    async translate(arg: unknown): Promise<void> {
       if (isString(arg)) {
         console.log("hello", arg);
       }
