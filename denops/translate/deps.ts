b9edd7d397693530009411e7e2d4538a7c804b85
skanehira <skanehira@users.noreply.github.com>
2023-01-06 01:10:19 +0000

    :package: Update Deno dependencies
    Update dependencies by udd:
    
    /home/runner/work/denops-translate.vim/denops-translate.vim/denops/translate/deps.ts
    [1/11] Looking for releases: https://deno.land/x/denops_std@v3.12.1/mod.ts
    [1/11] Using latest: https://deno.land/x/denops_std@v3.12.1/mod.ts
    [2/11] Looking for releases: https://deno.land/x/denops_std@v3.12.1/variable/mod.ts
    [2/11] Using latest: https://deno.land/x/denops_std@v3.12.1/variable/mod.ts
    [3/11] Looking for releases: https://deno.land/x/unknownutil@v2.1.0/mod.ts
    [3/11] Using latest: https://deno.land/x/unknownutil@v2.1.0/mod.ts
    [4/11] Looking for releases: https://deno.land/std@0.170.0/testing/asserts.ts
    [4/11] Attempting update: https://deno.land/std@0.170.0/testing/asserts.ts -> 0.171.0
    [4/11] Update successful: https://deno.land/std@0.170.0/testing/asserts.ts -> 0.171.0
    [5/11] Looking for releases: https://deno.land/x/denops_std@v3.12.1/test/mod.ts
    [5/11] Using latest: https://deno.land/x/denops_std@v3.12.1/test/mod.ts
    [6/11] Looking for releases: https://deno.land/x/denops_std@v3.12.1/variable/mod.ts
    [6/11] Using latest: https://deno.land/x/denops_std@v3.12.1/variable/mod.ts
    [7/11] Looking for releases: https://deno.land/x/denops_std@v3.12.1/mapping/mod.ts
    [7/11] Using latest: https://deno.land/x/denops_std@v3.12.1/mapping/mod.ts
    [8/11] Looking for releases: https://deno.land/x/denops_std@v3.12.1/mapping/types.ts
    [8/11] Using latest: https://deno.land/x/denops_std@v3.12.1/mapping/types.ts
    [9/11] Looking for releases: https://deno.land/x/xdg@v10.5.1/src/mod.deno.ts
    [9/11] Using latest: https://deno.land/x/xdg@v10.5.1/src/mod.deno.ts
    [10/11] Looking for releases: https://deno.land/std@0.170.0/path/mod.ts
    [10/11] Attempting update: https://deno.land/std@0.170.0/path/mod.ts -> 0.171.0
    [10/11] Update successful: https://deno.land/std@0.170.0/path/mod.ts -> 0.171.0
    [11/11] Looking for releases: https://deno.land/x/gtr@v0.0.1/mod.ts
    [11/11] Using latest: https://deno.land/x/gtr@v0.0.1/mod.ts
    
    Already latest version:
    https://deno.land/x/denops_std@v3.12.1/mod.ts == v3.12.1
    https://deno.land/x/denops_std@v3.12.1/variable/mod.ts == v3.12.1
    https://deno.land/x/unknownutil@v2.1.0/mod.ts == v2.1.0
    https://deno.land/x/denops_std@v3.12.1/test/mod.ts == v3.12.1
    https://deno.land/x/denops_std@v3.12.1/variable/mod.ts == v3.12.1
    https://deno.land/x/denops_std@v3.12.1/mapping/mod.ts == v3.12.1
    https://deno.land/x/denops_std@v3.12.1/mapping/types.ts == v3.12.1
    https://deno.land/x/xdg@v10.5.1/src/mod.deno.ts == v10.5.1
    https://deno.land/x/gtr@v0.0.1/mod.ts == v0.0.1
    
    Successfully updated:
    https://deno.land/std@0.170.0/testing/asserts.ts 0.170.0 -> 0.171.0
    https://deno.land/std@0.170.0/path/mod.ts 0.170.0 -> 0.171.0

--- denops/translate/deps.ts
+++ denops/translate/deps.ts
@@ -1,11 +1,11 @@
 export * from "https://deno.land/x/denops_std@v3.12.1/mod.ts";
 export * from "https://deno.land/x/denops_std@v3.12.1/variable/mod.ts";
 export * from "https://deno.land/x/unknownutil@v2.1.0/mod.ts";
-export * from "https://deno.land/std@0.170.0/testing/asserts.ts";
+export * from "https://deno.land/std@0.171.0/testing/asserts.ts";
 export * from "https://deno.land/x/denops_std@v3.12.1/test/mod.ts";
 export * as vars from "https://deno.land/x/denops_std@v3.12.1/variable/mod.ts";
 export * as mapping from "https://deno.land/x/denops_std@v3.12.1/mapping/mod.ts";
 export * from "https://deno.land/x/denops_std@v3.12.1/mapping/types.ts";
 export { default as xdg } from "https://deno.land/x/xdg@v10.5.1/src/mod.deno.ts";
-export * as path from "https://deno.land/std@0.170.0/path/mod.ts";
+export * as path from "https://deno.land/std@0.171.0/path/mod.ts";
 export * from "https://deno.land/x/gtr@v0.0.1/mod.ts";
